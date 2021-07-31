ebdddba14347bbdaae71852e50131a7cd636bfe5
Ittangihala Veeresha Veeresha Ittangihala <veeresh@iii.org.tw>
2021-07-31 22:49:44 +0800

    tests updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ="src", 
+    version="0.0.1", 
+    description="it's a wine Q package",
+    author = "veeruamma",
+    packages=find_packages(),
+    license="MIT"
+)
+
